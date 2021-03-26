205a516df05adbcba37b0b470125976d5a9dfdcc
hikaru-webcamp <79980351+hikaru-webcamp@users.noreply.github.com>
2021-03-26 12:30:33 +0900

    Add files via upload

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b3d9e1b851d328c1f4176ffa5bc9b5150841eb7b1fa69120d3621432c78ec58f2e68ad211828ab97e75c0adef1d8d7dc14fd64156c159e7c33c26d90878cebd2'
